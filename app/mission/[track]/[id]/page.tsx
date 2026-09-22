aabec1e1d67ec1883d2c4db7c52cdbf503ab0658
Sajal Dubey <sajal1209@gmail.com>
2026-09-23 03:55:12 +0530

    Phase 1B: update app/mission/[track]/[id]/page.tsx

--- app/mission/[track]/[id]/page.tsx
+++ app/mission/[track]/[id]/page.tsx
@@ -1,6 +1,6 @@
 import { notFound } from "next/navigation";
 import { requireChatGPTUser } from "../../../chatgpt-auth";
-import { getMission, type LearningTrack } from "@/modules/curriculum/public";
+import { getMission, getMissionIdentity, type LearningTrack } from "@/modules/curriculum/public";
 import { loadStructuredMission } from "@/modules/content/server/load-structured-mission";
 import MissionRouteClient from "./MissionRouteClient";
 
@@ -16,12 +16,13 @@ export default async function MissionRoute({ params }: { params: Promise<{ track
   const track = trackBySlug[trackSlug];
   const missionId = Number(rawId) - 1;
   const mission = track && Number.isInteger(missionId) ? getMission(track, missionId) : undefined;
-  if (!track || !mission) notFound();
+  const missionIdentity = track && Number.isInteger(missionId) ? getMissionIdentity(track, missionId) : undefined;
+  if (!track || !mission || !missionIdentity) notFound();
 
   const [user, structuredMission] = await Promise.all([
     requireChatGPTUser(`/mission/${trackSlug}/${rawId}`),
-    loadStructuredMission(track, missionId),
+    loadStructuredMission(missionIdentity.stableId),
   ]);
 
-  return <MissionRouteClient track={track} mission={mission} structuredMission={structuredMission} user={{ displayName: user.displayName, email: user.email }} />;
+  return <MissionRouteClient track={track} mission={mission} missionIdentity={missionIdentity} structuredMission={structuredMission} user={{ displayName: user.displayName, email: user.email }} />;
 }
